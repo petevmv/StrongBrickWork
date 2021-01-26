9b64165b5a9d32b3b8389f8deae5822607a60f95
Maksim Petev <petevmv@gmail.com>
2021-01-26 19:14:19 +0200

    fixed input validation when its bigger than the actual dimensin (NxM)

--- strongbrickwork.py
+++ strongbrickwork.py
@@ -5,35 +5,41 @@ while exit_loop:
     try:
         N , M = map(int,(input("Enter dimenson N and number M as even integer no greatar than 100 :").split()))
     except :
+        print("==Please Enter valid dimension==")
         continue
     if (N % 2) == 0 and (M % 2) == 0 and N > 0 and N <= 100 and M > 0 and M <= 100:
         exit_loop = False
-N_list = []
-M_list = []
-for i in range(N):
-    N_list.append(input("Enter bricks possition separeted by space: ").split())
 
+N_list = []
+for i in range(N):
+    N_list.append(input("Enter numbers to form bricks separeted by space: ").split())
+    print(N_list[i])
+    if len(N_list[i]) != M:
+        print("You have entered more elements than required based on you'r {}x{} input, entering first {} of them".format(N, M, M))
+        N_list[i] = N_list[i][0:M]
+        print(N_list)
 arr = np.array(N_list, dtype=int)
-print("Layer 1 \n",arr)
+print("Layer 1 (input) \n",arr)
 arr_2 = np.zeros((N, M),dtype=int)
 
-
-for i in range(0, N):
-    for j in range(0, M):
-        if arr_2[i,j] != 0:
-            continue
-        if j == len(arr_2[i]) - 1:
-            arr_2[i,j] = arr[i+1,0]
-            arr_2[i+1,j] = arr[i+1,0]
-            break
-        if arr[i,j] == arr[i,j+1]:
-            arr_2[i,j] = arr[i,j]
-            arr_2[i+1,j] = arr[i,j]
-        else:
-            brick = arr[i,j]
-            if brick in arr_2:
-                brick = arr[i,j+1]
-            arr_2[i,j] = brick
-            arr_2[i,j+1] = brick
-
-print("Layer 2 \n", arr_2)
+try:
+    for i in range(0, N):
+        for j in range(0, M):
+            if arr_2[i,j] != 0:
+                continue
+            if j == len(arr_2[i]) - 1:
+                arr_2[i,j] = arr[i+1,0]
+                arr_2[i+1,j] = arr[i+1,0]
+                break
+            if arr[i,j] == arr[i,j+1]:
+                arr_2[i,j] = arr[i,j]
+                arr_2[i+1,j] = arr[i,j]
+            else:
+                brick = arr[i,j]
+                if brick in arr_2:
+                    brick = arr[i,j+1]
+                arr_2[i,j] = brick
+                arr_2[i,j+1] = brick
+except:
+    print("-1 \n No solution found")
+print("Layer 2 (output) \n", arr_2)
